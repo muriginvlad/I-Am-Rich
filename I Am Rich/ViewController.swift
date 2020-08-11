ea4357c722e8d0d019a6e7a5ae2d0e1b7868865a
Vladislav <muriginvlad@gmail.com>
2020-08-11 21:33:03 +0300

    first commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Владислав on 25/06/2019.
+//  Copyright © 2019 Murygin Vladislav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
